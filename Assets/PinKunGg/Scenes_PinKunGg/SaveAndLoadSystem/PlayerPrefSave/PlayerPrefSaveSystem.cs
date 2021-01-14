112c7da81d77064815d6bed1b497c327fe948063
PinKunGg <70133896+PinKunGg@users.noreply.github.com>
2021-01-15 00:35:45 +0700

    nanimo

---
+++ Assets/PinKunGg/Scenes_PinKunGg/SaveAndLoadSystem/PlayerPrefSave/PlayerPrefSaveSystem.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPrefSaveSystem : MonoBehaviour
+{
+    //Save Location = Computer\HKEY_CURRENT_USER\SOFTWARE\Unity\UnityEditor\DefaultCompany\GameProject
+    public static PlayerPrefSaveSystem PPSSInstanse;
+    public float playerPosX,playerPosY,playerPosZ;
+    private void Awake()
+    {
+        PPSSInstanse = this;
+
+        playerPosX = PlayerPrefs.GetFloat("PlayerPositionX");
+        playerPosY = PlayerPrefs.GetFloat("PlayerPositionY");
+        playerPosZ = PlayerPrefs.GetFloat("PlayerPositionZ");
+    }
+    public void RecivePlayerPos(Vector3 playerPos)
+    {
+        SavePos(playerPos.x,playerPos.y,playerPos.z);
+    }
+    public Vector3 ExportPlayerPos()
+    {
+        return new Vector3(playerPosX,playerPosY,playerPosZ);
+    }
+    public void SavePos(float valueX, float valueY, float valueZ)
+    {
+        print("*--* Save PlayerPrefs *--*");
+        PlayerPrefs.SetFloat("PlayerPositionX",valueX);
+        PlayerPrefs.SetFloat("PlayerPositionY",valueY);
+        PlayerPrefs.SetFloat("PlayerPositionZ",valueZ);
+        
+    }
+    public void LoadPos()
+    {
+        print("*--* Load PlayerPrefs *--*");
+        playerPosX = PlayerPrefs.GetFloat("PlayerPositionX",playerPosX);
+        playerPosY = PlayerPrefs.GetFloat("PlayerPositionY",playerPosY);
+        playerPosZ = PlayerPrefs.GetFloat("PlayerPositionZ",playerPosZ);
+    }
+}
