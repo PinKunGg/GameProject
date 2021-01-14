f8cd943498731fbdb09007ca9846aab71756fcc3
PinKunGg <70133896+PinKunGg@users.noreply.github.com>
2021-01-14 23:55:57 +0700

    New Brach "PinKunGg"

---
+++ Assets/Script/BuildingSystem/BuildingData.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Data/Building Data")]
+public class BuildingData : ScriptableObject
+{
+    public GameObject Building;
+    public Vector2 BuildingZoneSize;
+    public int BuildingType;
+    public bool isGroundBuildCon;
+    public bool isWaterBuildCon;
+}
